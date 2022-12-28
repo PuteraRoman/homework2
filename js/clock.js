2c813ebf08cf513564086e71b37e094e92456728
Putera Roman <116205862+Cryemeri@users.noreply.github.com>
2022-12-28 13:24:42 +0000

    Clock

--- js/clock.js
+++ js/clock.js
@@ -3,6 +3,8 @@ function updateClock(){
     const clockContainer = document.querySelector('.clock');
 clockContainer.innerText = new Date().toLocaleTimeString();
 clockContainer.style.color = "blue";
+clockContainer.style.border = "dashed blue";
+clockContainer.style.weight = "20px";
 }
 setInterval(updateClock,1000)
 })()
